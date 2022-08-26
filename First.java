49b90a89ff5a93f3320f33875c7f389a6608a4bc
21A91A6153 <97424603+21A91A6153@users.noreply.github.com>
2022-08-26 10:49:18 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,47 @@
+class First
+{ 
+  public static void main(String args[])
+   {
+     System.out.print("hi");
+   }
+}
+/*
+ import java.util.Scanner;--->for reading info
+
+datatypes:
+byte-nextByte();  -
+short-nextShort();  - 
+int-nextInt();         - integer datatypes
+long-nextLong();    -
+  
+char -nextLine().charAt(0);------ first it checks string and gives character at index;
+
+float -nextFloat();
+double - nextDouble();
+
+string-nextLine();
+
+output-System.out-->standard output device-monitor;
+System.in->standard input device-keyboard;
+System.out.println();
+
+
+1.Display message:
+System.out.println("welcome");
+------->welcome
+
+2.Display value of variable:
+System.out.println(variable);
+ex:
+  int a=10;
+  System.out.println(a);
+  ----->10
+
+3.Display Message+value of a variable.
+System.out.println("Message"+variable);
+ex:
+  int a=10;
+  System.out.println("a"+a);
+  ---->a=10
+
+*/
